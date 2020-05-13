ccc234ae9947498d8bba23d9a047606208cd84c6
baydarov <egbaydarov@edu.hse.ru>
2020-05-13 06:31:05 +0300

    add key path config

--- src/download.py
+++ src/download.py
@@ -3,6 +3,11 @@ import requests
 def download_image():
 	url1 ="https://www.freeopenvpn.org/logpass/russia.php"
 
+
+	with open('config', 'r') as f:
+	         url1 = f.readline().split()[0]
+	
+
 	payload = {}
 	headers = {
 	  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
--- src/main.py
+++ src/main.py
@@ -6,5 +6,8 @@ download_image()
 detected_cap = Captcha_detection("pass.png")
 with open("pass.txt", "w") as myfile:
 	myfile.write('freeopenvpn\n' + detected_cap)
+keypath = ''
+with open('config', 'r') as f:
+	keypath = f.readline().split()[1]
 
-os.system('cmd /c "openvpn --config 1.key --auth-user-pass pass.txt"')
+os.system('cmd /c "openvpn --config ' + keypath + ' --auth-user-pass pass.txt"')
